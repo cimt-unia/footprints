911f330141ed88a68d7fcd5cfa1a15451423f1d4
korbinian <korbinianfl98@googlemail.com>
2026-01-22 11:57:37 +0100

    feat: lang settings

--- src/components/experiment/Instruction.svelte
+++ src/components/experiment/Instruction.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import { LengthState } from "$lib/length_state.js";
-    import TestRunner from "$routes/experiment/components/TestRunner.svelte";
-    import { Modal } from "@skeletonlabs/skeleton-svelte";
+	import { LengthState } from "$lib/length_state.js";
+	import TestRunner from "$routes/experiment/components/TestRunner.svelte";
+	import { Modal } from "@skeletonlabs/skeleton-svelte";
+	import { _ } from "svelte-i18n";
 
 	interface Props {
 		cb: () => Promise<void>;
@@ -42,172 +43,54 @@
 </script>
 
 <div class="flex flex-col m-auto max-w-4xl text-lg justify-between">
-	<h1 class="h1 mb-10 mt-10">Versuchsbeschreibung</h1>
+	<h1 class="h1 mb-10 mt-10">{$_("instructions.h1")}</h1>
 	{#if step === 0}
-		<b>Willkommen zu unserem Experiment</b>
-
-		<p class="mt-2">
-			In diesem Experiment möchten wir biomechanische Parameter
-			aufzeichnen, um Ihren Gang zu beschreiben. Zu diesem Zweck bitten
-			wir Sie, den markierten Flur auf und ab zu gehen.
-		</p>
-
-		<p class="mt-4">Jede Runde entspricht einem Durchgang.</p>
-
-		<p class="mt-4">
-			Zu Beginn gibt es eine kurze Vorbereitungsphase, in der Sie ein
-			Fixationskreuz auf dem Bildschirm betrachten sollen. Darauf folgt
-			die Präsentation eines Bildes (5 Sekunden), gefolgt von einem
-			Startsignal, das anzeigt, dass Sie mit dem Gehen beginnen sollen.
-		</p>
-		<p class="mt-4">
-			Wenn Sie das Ende des Weges erreichen, werden Sie gebeten, das Bild
-			danach zu bewerten, wie es auf Sie gewirkt hat.
-		</p>
+		<b>{$_("instructions.welcome.h2")}</b>
+		<p class="mt-2">{$_("instructions.welcome.p1")}</p>
+		<p class="mt-4">{$_("instructions.welcome.p2")}</p>
+		<p class="mt-4">{$_("instructions.welcome.p3")}</p>
+		<p class="mt-4">{$_("instructions.welcome.p4")}</p>
 	{:else if step === 1}
-		<h2 class="h2">Valenz / Positivität</h2>
+		<h2 class="h2">{$_("instructions.valence.h2")}</h2>
 
-		<p class="mt-4">
-			Bilder kommunizieren. Sie zeigen Menschen, Gegenstände und Szenen.
-			In dieser Studie interessiert uns, wie verschiedene Bilder Menschen
-			fühlen lassen.
-		</p>
-		<p class="mt-4">
-			Bitte denken Sie beim Beantworten daran: Es gibt keine richtigen
-			oder falschen Antworten. Wir sind an Ihrer individuellen Meinung
-			interessiert. Bitte verweilen Sie nicht zu lange bei einem Bild.
-			Ihre erste Antwort ist genauso gut wie jede andere.
-		</p>
-
-		<p class="mt-4">
-			Wir werden Sie bitten, eine Reihe von Bildern danach zu bewerten,
-			wie positiv oder negativ sie sind. Wenn das Bild etwas Gutes oder
-			Positives darstellt, verwenden Sie bitte die rechte Seite der Skala,
-			um Ihre Antwort zu markieren. Positive Bilder sind solche, die Dinge
-			darstellen, die uns glücklich, zufrieden, kompetent, stolz, erfüllt,
-			erfreut usw. machen. Es spielt keine Rolle, worum es in dem
-			speziellen Bild geht, solange es etwas Positives oder Gutes
-			darstellt.
-		</p>
-
-		<p class="mt-4">
-			Wenn das Bild etwas Schlechtes oder Negatives darstellt, verwenden
-			Sie bitte die linke Seite der Skala, um Ihre Antwort zu markieren.
-			Negative Bilder sind solche, die Dinge darstellen, die uns
-			unglücklich, verärgert, irritiert, wütend, traurig, deprimiert usw.
-			machen. Es spielt keine Rolle, worum es in dem speziellen Bild geht,
-			solange es etwas Negatives oder Schlechtes darstellt.
-		</p>
-
-		<p class="mt-4">
-			Verwenden Sie die Mitte der Skala, um anzugeben, dass das Bild Sie
-			neutral fühlen lässt, also weder positiv noch negativ. Bitte nutzen
-			Sie den gesamten Bereich der Skala für Ihre Antworten, anstatt sich
-			nur auf wenige Punkte zu beschränken.
-		</p>
+		<p class="mt-4">{$_("instructions.valence.p1")}</p>
+		<p class="mt-4">{$_("instructions.valence.p2")}</p>
+		<p class="mt-4">{$_("instructions.valence.p3")}</p>
+		<p class="mt-4">{$_("instructions.valence.p4")}</p>
+		<p class="mt-4">{$_("instructions.valence.p5")}</p>
 	{:else if step === 2}
-		<h2 class="h2">Arousal / Erregung</h2>
+		<h2 class="h2">{$_("instructions.arousal.h2")}</h2>
 
-		<p class="mt-4">
-			Wir werden Sie bitten, eine Reihe von Bildern danach zu bewerten,
-			wie stark die Emotionen sind, die sie hervorrufen. Anders gesagt,
-			wir möchten wissen, wie intensiv das Bild Emotionen erzeugt; es
-			spielt keine Rolle, ob das Bild etwas Gutes oder Schlechtes
-			darstellt. Uns interessiert nur das Ausmaß an Erregung, Energie oder
-			Intensität des Gefühls, das es repräsentiert.
-		</p>
-
-		<p class="mt-4">
-			Verwenden Sie die rechte Seite der Skala, wenn das Bild etwas
-			darstellt, das stark emotional ist. Wörter, die den emotionalen
-			Zustand beschreiben, den das Bild erzeugt, könnten z. B. „erregt“,
-			„aufmerksam“, „aktiviert“, „geladen“ oder „energetisiert“ sein.
-		</p>
-		<p class="mt-4">
-			Verwenden Sie die linke Seite der Skala, wenn das Bild etwas
-			darstellt, das nicht stark emotional ist. Wörter, die den
-			emotionalen Zustand beschreiben, könnten z. B. „nicht erregt“,
-			„langsam“, „ruhig“, „energielos“, „entspannt“ oder „friedlich“ sein.
-			Verwenden Sie die Mitte der Skala, um ein Bild zu kennzeichnen, das
-			mäßig erregend ist oder zwischen den beiden Extremen liegt.
-		</p>
-
-		<p class="mt-4">
-			Bitte nutzen Sie den gesamten Bereich der Skala für Ihre Antworten,
-			anstatt sich nur auf wenige Punkte zu beschränken. Beim Bewerten
-			sollen Sie vergessen, ob das Bild etwas Positives oder Negatives,
-			Gutes oder Schlechtes darstellt. Bewerten Sie das Bild
-			ausschließlich nach der Intensität der Emotionen, die es auslöst.
-		</p>
-
-		<p class="mt-4">
-			Beispiel: Sie haben zwei Bilder – eines zeigt einen Athleten, der
-			eine Goldmedaille gewinnt (etwas Gutes), und ein anderes zeigt einen
-			Athleten, der verletzt wird und das Rennen verliert (etwas
-			Schlechtes). Sie können beiden Bildern die gleiche Bewertung geben,
-			weil sie eine ähnliche emotionale Intensität erzeugen, auch wenn die
-			Gefühle unterschiedlich sind. Ihre Antworten sollen sich
-			ausschließlich danach richten, wie stark die Emotionen sind, die das
-			Bild hervorruft.
-		</p>
+		<p class="mt-4">{$_("instructions.arousal.p1")}</p>
+		<p class="mt-4">{$_("instructions.arousal.p2")}</p>
+		<p class="mt-4">{$_("instructions.arousal.p3")}</p>
+		<p class="mt-4">{$_("instructions.arousal.p4")}</p>
+		<p class="mt-4">{$_("instructions.arousal.p5")}</p>
 	{:else if step === 3}
-		<h2 class="h2">Ablauf</h2>
+		<h2 class="h2">{$_("instructions.procedure.h2")}</h2>
 
-		<h4 class="h4 mt-4">Phase 1: Fixationskreuz</h4>
-		<p class="mt-4 mb-4">
-			Sie stehen in <b>der Mitte</b> der beiden Bildschirme an der
-			Tastatur. Ihnen wird für eine kurze Zeit ein Fixationskreuz gezeigt,
-			bitte konzentrieren Sie sich auf das <b>Kreuz</b>.
-		</p>
-		<h4 class="h4">Phase 2: Stimulation</h4>
-		<p class="mt-4 mb-4">
-			Ihnen wird für eine kurze Zeit das Bild dieses Durchlaufes gezeigt.
-		</p>
-		<h4 class="h4">Phase 3: Bewegung</h4>
-		<p class="mt-4">
-			Durch ein Signal, <b>auditiv und visuell</b>, wird Ihnen
-			signalisiert, dass Sie loslaufen sollen. Die
-			<b>Zielgeschwindigkeit</b>
-			wird Ihnen durch ein
-			<b>animiertes Männchen</b> vorgegeben. Die <b>Geschwindigkeit</b>
-			kann von Durchlauf zu Durchlauf <b>variieren</b>.
-		</p>
-		<p class="mt-4 mb-4">
-			Sind Sie bei einer <b>Markierung</b>, kurz vor den Bildschirmen,
-			angekommen haben Sie <b>3 Sekunden Zeit sich umzudrehen</b>. Nach
-			diesen 3 Sekunden wird Ihnen wieder durch ein <b>Audiosignal</b>,
-			sowie das Männchen,, signalisiert, dass Sie
-			<b>loslaufen</b> sollen.
-			<br />
-			Dieser Ablauf wiederholt sich <b>2 mal</b>. Bleiben Sie nach dem 2.
-			Mal in der
-			<b>Mitte</b> stehen.
-		</p>
-		<h4 class="h4">Phase 4: Bewertung</h4>
-		<p class="mt-4 mb-4">
-			Sie werden nun nach Ihrer Bewertung gefragt. Die <b>Skala</b>
-			umfasst einen Bereich von <br /> <b>1-7</b> (1: niedrig/negativ, 7:
-			hoch/positiv); nutzen Sie die
-			<b>Zahlen auf der Tastatur</b>
-			zum Bewerten. Um ihre Bewertung zu <b>Bestätigen</b> drücken Sie
-			<b>Enter</b>.
-			<br />
-			Nach dem Bewerten startet der nächste Durchlauf mit <b>Phase 1</b>.
-		</p>
+		<h4 class="h4 mt-4">{$_("instructions.procedure.phase_1.h2")}</h4>
+		<p class="mt-4 mb-4">{@html $_("instructions.procedure.phase_1.p1")}</p>
+		<h4 class="h4 mt-4">{$_("instructions.procedure.phase_2.h2")}</h4>
+		<p class="mt-4 mb-4">{@html $_("instructions.procedure.phase_2.p1")}</p>
+		<h4 class="h4">{$_("instructions.procedure.phase_3.h2")}</h4>
+		<p class="mt-4">{@html $_("instructions.procedure.phase_3.p1")}</p>
+		<p class="mt-4 mb-4">{@html $_("instructions.procedure.phase_3.p2")}</p>
+		<h4 class="h4">{$_("instructions.procedure.phase_4.h2")}</h4>
+		<p class="mt-4 mb-4">{@html $_("instructions.procedure.phase_4.p1")}</p>
 	{:else if step === 4}
-		<h4 class="h4">Triggerwarnung</h4>
+		<h4 class="h4">{$_("instructions.trigger.h2")}</h4>
 		<p class="mt-4 mb-4">
-			Manche Bilder können auf manche Menschen verstörend wirken (z.B.
-			Krieg, Gewalt, Verletzungen, Nacktheit oder bei manchen Personen
-			Ängste auslösende Darstellungen).
+			{$_("instructions.trigger.p1")}
 		</p>
-
-		<b>Bereit? Drücke Enter um das Experiment zu starten. Drücke T für einen Testdurchlauf.</b>
+		<b>
+			{$_("instructions.trigger.b")}
+		</b>
 	{/if}
 </div>
 <Modal
 	open={test_open}
-	onOpenChange={(e) => (test_open= e.open)}
+	onOpenChange={(e) => (test_open = e.open)}
 	contentBase="card bg-surface-100-900 space-y-4 shadow-xl min-w-screen min-h-screen"
 	backdropClasses="backdrop-blur-sm"
 >
